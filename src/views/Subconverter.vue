efafdfe07653ddd9a2b97e28982896a63d634d8a
yigoox <92346614+yigoox@users.noreply.github.com>
2021-11-27 17:15:05 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://clash.cxixcheroku.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
